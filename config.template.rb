3ab63052b9e798aa2f160be27d239be307a018b9
Nathan Hwang <beta.entity.k@gmail.com>
2012-04-13 20:43:41 -0400

    Have pulls from gcal

---
+++ config.template.rb
@@ -0,0 +1,10 @@
+# configuration of the doorbell
+
+module Config
+  # gcal things
+  FEED_URL = ""
+
+  # twilio things
+  TWILIO_API_KEY = ""
+  TWILIO_NUMBER = ""
+end
--- doorbell.rb
+++ doorbell.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'net/http'
+require 'uri'
+require 'xml'
+
+require 'config'
 
 get '/' do
-  "Hello world"
+  # get our current time
+  t = Time.now.utc
+
+  # get the events
+  feed_uri = URI.parse(Config::FEED_URL)
+  data = Net::HTTP.get(feed_uri.host, feed_uri.path)
+  # parse up the XML
+  
+  # go through each event, see if we're 
+  
 end
